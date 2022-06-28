390f027d835b189cb4a887b4fc93f2aba8960229
CurtisHigh <105828905+CurtisHigh@users.noreply.github.com>
2022-06-28 00:55:14 -0500

    add pokemon list array and populate with 3 entries

--- js/script.js
+++ js/script.js
@@ -1,4 +1,19 @@
-let favoriteFood = 'Apples';
-document.write(favoriteFood);
+pokemonList = [];
 
-alert('Hello world');
+pokemonList[0] = {
+  name: 'Bulbasaur',
+  height: 0.7,
+  types: ['monster','grass']
+};
+
+pokemonList[1] = {
+  name: 'Charmander',
+  height: 0.6,
+  types: ['monster','dragon']
+};
+
+pokemonList[2] = {
+  name: 'Squirtle',
+  height: 0.5,
+  types: ['monster','water']
+};
