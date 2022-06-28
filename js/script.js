8066b306b50fb7a1d0465e3fe92df165324875f1
CurtisHigh <105828905+CurtisHigh@users.noreply.github.com>
2022-06-28 01:02:13 -0500

    add pokemonList array and entries

--- js/script.js
+++ js/script.js
@@ -1,19 +1,17 @@
-pokemonList = [];
-
-pokemonList[0] = {
-  name: 'Bulbasaur',
-  height: 0.7,
-  types: ['monster','grass']
-};
-
-pokemonList[1] = {
-  name: 'Charmander',
-  height: 0.6,
-  types: ['monster','dragon']
-};
-
-pokemonList[2] = {
-  name: 'Squirtle',
-  height: 0.5,
-  types: ['monster','water']
-};
+let pokemonList = [
+  {
+    name: 'Bulbasaur',
+    height: 0.7,
+    types: ['monster','grass']
+  },
+  {
+    name: 'Charmander',
+    height: 0.6,
+    types: ['monster','dragon']
+  },
+  {
+    name: 'Squirtle',
+    height: 0.5,
+    types: ['monster','water']
+  }
+];
